7aec166c0ac5e3a3fbfdc3e2f2ee442fb0414767
Tom Bredemeier <tbredemeier@barrington220.org>
2026-02-19 10:36:19 -0600

    Initial Commit

---
+++ API Poetry/API_PoetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_PoetryApp.swift
+//  API Poetry
+//
+//  Created by Tom Bredemeier on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct API_PoetryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API Poetry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  API Poetry
+//
+//  Created by Tom Bredemeier on 2/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
